bba623961e104cd157be596c63b14f1d9b3665eb
ramirezr69 <ronramirez@patricks-imac>
2016-08-06 11:48:40 -0500

    Initial Commit

---
+++ drawing-with-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drawing-with-ios
+//
+//  Created by Ron Ramirez on 8/6/16.
+//  Copyright © 2016 Mochi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

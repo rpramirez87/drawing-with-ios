af1def783082c95eeb114714a19a19dd974fb3ba
ramirezr69 <ronramirez@patricks-imac.attlocal.net>
2016-08-06 19:32:43 -0500

    Implemented Color Buttons RGBY and Random

--- drawing-with-ios/DrawingViewController.swift
+++ drawing-with-ios/DrawingViewController.swift
@@ -14,9 +14,12 @@ class DrawingViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var lastPoint = CGPoint.zero
+    var red : CGFloat = 0
+    var green : CGFloat = 0
+    var blue : CGFloat = 0
     
     
-    
+    // MARK: View Controller Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,12 +66,20 @@ class DrawingViewController: UIViewController {
             self.imageView.image?.drawInRect(CGRect(x:0,y:0,width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
             CGContextMoveToPoint(context, lastPoint.x, lastPoint.y)
             CGContextAddLineToPoint(context, point.x, point.y)
+            
+            // Stroke Color
+            CGContextSetRGBStrokeColor(context, self.red, self.green, self.blue, 1.0)
+            
+            // Line Size - Round/Square
+            CGContextSetLineCap(context, .Round)
+            
+            // Line Width
+            CGContextSetLineWidth(context, 15)
             CGContextStrokePath(context)
             
             self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
             
             UIGraphicsEndImageContext()
-            
             
             self.lastPoint = point
         }
@@ -95,6 +106,41 @@ class DrawingViewController: UIViewController {
     func clear() {
         self.imageView.image = nil
     }
+    
+    // MARK: Color Button Functions
+    @IBAction func blueButtonTapped(sender: AnyObject) {
+        print("Blue")
+        self.red = 60 / 255
+        self.green = 112 / 255
+        self.blue = 226 / 255
+    }
+    @IBAction func greenButtonTapped(sender: AnyObject) {
+        print("Green")
+        self.red = 54 / 255
+        self.green = 233 / 255
+        self.blue = 119 / 255
+    }
+    
+    @IBAction func redButtonTapped(sender: AnyObject) {
+        print("Red")
+        self.red = 226 / 255
+        self.green = 58 / 255
+        self.blue = 62 / 255
+    }
+    @IBAction func yellowButtonTapped(sender: AnyObject) {
+        self.red = 248 / 255
+        self.green = 213 / 255
+        self.blue = 55 / 255
+        print("Yellow")
+    }
+    @IBAction func randomButtonTapped(sender: AnyObject) {
+        print("Random")
+        self.red = CGFloat(arc4random_uniform(255)) / 255
+        self.green = CGFloat(arc4random_uniform(255)) / 255
+        self.blue = CGFloat(arc4random_uniform(255)) / 255
+        
+    }
+    
 }
 
 
